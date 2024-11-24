6cea29eee14e89a506e212225e3a904f4870b499
dspedroborges <xpedrostewart@gmail.com>
2024-11-24 04:09:40 -0300

    legendas e responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import { Suspense, useEffect, useState } from 'react';
 import { Gloria_Hallelujah } from "next/font/google";
-import { BsArrowLeft, BsArrowRight, BsPlusCircle, BsTrash } from 'react-icons/bs';
+import { BsArrowLeft, BsArrowRight, BsClipboard2, BsClipboard2Check, BsPlusCircle, BsTrash } from 'react-icons/bs';
 
 const gloriaHallelujah = Gloria_Hallelujah({ weight: ["400"], subsets: ["latin"] });
 
 const perc = (part: number, whole: number) => {
-  if (isNaN(part/whole)) return "0%";
-  return `${((part/whole)*100).toFixed(2)}%`;
+  if (isNaN(part / whole)) return "0%";
+  return `${((part / whole) * 100).toFixed(2)}%`;
 }
 
 export default function Home() {
@@ -31,7 +31,13 @@ function Page() {
   let complete = searchParams.get("complete");
   const router = useRouter();
   const [showAddTask, setShowAddTask] = useState(false);
-  const [currentTitle, setCurrentTitle] = useState(title || "");
+  const [currentTitle, setCurrentTitle] = useState(title || "Altere o título");
+  const [addedClipboard, setAddedClipboard] = useState(false);
+  const [showAddLegend, setShowAddLegend] = useState(false);
+
+  const addToClipboard = () => {
+    navigator.clipboard.writeText(window.location.href);
+  }
 
   const [categories, setCategories] = useState<Record<string, string[]>>({
     legend: [],
@@ -56,6 +62,7 @@ function Page() {
   }, [categories, currentTitle]);
 
   const maxRows = Math.max(
+    categories.legend.length,
     categories.fridge.length,
     categories.emergency.length,
     categories.progress.length,
@@ -79,26 +86,33 @@ function Page() {
 
   return (
     <main className="p-4 bg-gradient-to-b from-black to-gray-800 min-h-screen">
-      <table className="mx-auto mt-8 w-full text-[8px] lg:text-base text-white">
+      <table className="hidden lg:table mx-auto mt-8 w-full text-[8px] lg:text-base text-white">
         <thead>
           <tr>
-            <th colSpan={5} className="border p-4 rounded-xl">
-              <input type="text" className={`bg-transparent text-white text-center rounded-xl text-2xl lg:text-6xl ${gloriaHallelujah.className}`} value={currentTitle || "Altere o título"} onChange={(e) => setCurrentTitle(e.target.value)} />
+            <th colSpan={6} className="border p-4 rounded-xl">
+              <input type="text" className={`bg-transparent text-white text-center rounded-xl text-2xl lg:text-6xl ${gloriaHallelujah.className}`} value={currentTitle} onChange={(e) => setCurrentTitle(e.target.value)} />
             </th>
           </tr>
           <tr>
-            {/* <th className="border p-4">Legenda</th> */}
-            <th className="border p-4">Geladeira</th>
-            <th className="border p-4 bg-red-600 text-white">Emergência</th>
-            <th className="border p-4">Progresso</th>
-            <th className="border p-4">Teste</th>
-            <th className="border p-4 bg-green-600 text-white">Completo</th>
+            <th className="border p-4">Legenda</th>
+            <th className="border p-4 bg-blue-800 text-white">Geladeira</th>
+            <th className="border p-4 bg-red-800 text-white">Emergência</th>
+            <th className="border p-4 bg-yellow-800 text-white">Progresso</th>
+            <th className="border p-4 bg-purple-800 text-white">Teste</th>
+            <th className="border p-4 bg-green-800 text-white">Completo</th>
           </tr>
         </thead>
         <tbody>
           {Array.from({ length: maxRows }).map((_, rowIndex) => (
             <tr key={rowIndex}>
-              {/* <td className="border p-4">{categories.legend[rowIndex] || ""}</td> */}
+              <td className="border p-4">
+                <Legend
+                  name={categories.legend[rowIndex] || ""}
+                  currentCategory="legend"
+                  rowIndex={rowIndex}
+                  removeElement={removeElement}
+                />
+              </td>
               <td className="border p-4">
                 <Controls
                   name={categories.fridge[rowIndex] || ""}
@@ -157,11 +171,136 @@ function Page() {
         </tbody>
         <tfoot>
           <tr>
-            {/* <td className="border p-4">Adicionar</td> */}
-            <td colSpan={6} className="text-center border p-4 cursor-pointer" onClick={() => setShowAddTask(true)}><BsPlusCircle className="inline text-4xl hover:scale-95" /></td>
+            <td className="text-center border p-4 cursor-pointer" onClick={() => setShowAddLegend(true)}><BsPlusCircle className="inline text-4xl hover:scale-95" /></td>
+            <td colSpan={5} className="text-center border p-4 cursor-pointer" onClick={() => setShowAddTask(true)}><BsPlusCircle className="inline text-4xl hover:scale-95" /></td>
           </tr>
         </tfoot>
       </table>
+
+      {/* start responsive block */}
+      <div className="lg:hidden">
+        <div className="border rounded-xl my-8 pb-4">
+          <h3 className="text-white rounded-t-xl p-4 text-center text-2xl font-bold bg-black">Legenda</h3>
+          {
+            categories.legend.map((elem, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <Legend
+                    name={elem}
+                    currentCategory="legend"
+                    rowIndex={i}
+                    removeElement={removeElement}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className="border rounded-xl my-8 pb-4">
+          <h3 className="text-white rounded-t-xl p-4 text-center text-2xl font-bold bg-blue-800">Geladeira</h3>
+          {
+            categories.fridge.map((elem, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <Controls
+                    name={elem}
+                    currentCategory="fridge"
+                    nextCategory="emergency"
+                    rowIndex={i}
+                    changeCategory={changeCategory}
+                    removeElement={removeElement}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className="border rounded-xl my-8 pb-4">
+          <h3 className="text-white rounded-t-xl p-4 text-center text-2xl font-bold bg-red-800">Emergência</h3>
+          {
+            categories.emergency.map((elem, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <Controls
+                    name={elem}
+                    currentCategory="emergency"
+                    previousCategory="fridge"
+                    nextCategory="progress"
+                    rowIndex={i}
+                    changeCategory={changeCategory}
+                    removeElement={removeElement}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className="border rounded-xl my-8 pb-4">
+          <h3 className="text-white rounded-t-xl p-4 text-center text-2xl font-bold bg-yellow-800">Progresso</h3>
+          {
+            categories.progress.map((elem, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <Controls
+                    name={elem}
+                    currentCategory="progress"
+                    previousCategory="emergency"
+                    nextCategory="test"
+                    rowIndex={i}
+                    changeCategory={changeCategory}
+                    removeElement={removeElement}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className="border rounded-xl my-8 pb-4">
+          <h3 className="text-white rounded-t-xl p-4 text-center text-2xl font-bold bg-purple-800">Teste</h3>
+          {
+            categories.test.map((elem, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <Controls
+                    name={elem}
+                    currentCategory="test"
+                    previousCategory="progress"
+                    nextCategory="complete"
+                    rowIndex={i}
+                    changeCategory={changeCategory}
+                    removeElement={removeElement}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className="border rounded-xl my-8 pb-4">
+          <h3 className="text-white rounded-t-xl p-4 text-center text-2xl font-bold bg-green-800">Completo</h3>
+          {
+            categories.complete.map((elem, i) => {
+              return (
+                <div key={i} className="my-4">
+                  <Controls
+                    name={elem}
+                    currentCategory="complete"
+                    previousCategory="test"
+                    rowIndex={i}
+                    changeCategory={changeCategory}
+                    removeElement={removeElement}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      {/* end responsive block */}
 
       <div className="text-white border rounded-xl p-4 mt-8">
         <p className="text-center my-4 font-bold lg:text-2xl">Progresso: {perc(categories.complete.length, (categories.complete.length + categories.fridge.length + categories.emergency.length + categories.progress.length + categories.test.length))}</p>
@@ -176,14 +315,19 @@ function Page() {
               <form
                 action={(formData: FormData) => {
                   const task = formData.get("task") as string;
-                  setCategories({ ...categories, fridge: [...categories.fridge, task] });
+                  const color = formData.get("color") as string;
+                  setCategories({ ...categories, fridge: [...categories.fridge, `${color}>>${task}`] });
                   setShowAddTask(false);
                 }}
                 className="flex flex-col gap-4"
               >
                 <div className="flex flex-col gap-2">
                   <label className="font-bold cursor-pointer" htmlFor="task">Tarefa</label>
-                  <input type="text" name="task" id="task" className="rounded-xl p-2 border bg-black border outline-blue-800" />
+                  <input type="text" name="task" id="task" className="rounded-xl p-2 border bg-black outline-blue-800" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="font-bold cursor-pointer" htmlFor="color">Cor</label>
+                  <input type="color" name="color" id="color" className="w-full bg-black outline-blue-800 h-12 cursor-pointer hover:scale-95" />
                 </div>
                 <button className="bg-blue-900 hover:bg-blue-800 rounded-full px-4 py-2 text-white w-full">Adicionar</button>
               </form>
@@ -192,27 +336,69 @@ function Page() {
         )
       }
 
-      {/* <form>
-        <div className="flex flex-col gap-2">
-          <label className="font-bold cursor-pointer" htmlFor="legend">Legenda</label>
-          <input type="legend" name="legend" id="legend" className="rounded-xl p-2 border outline-blue-800" />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label className="font-bold cursor-pointer" htmlFor="color">Cor</label>
-          <input type="color" name="color" id="color" />
-        </div>
-        <button className="bg-blue-900 hover:bg-blue-800 rounded-full px-4 py-2 text-white">Adicionar</button>
-      </form> */}
+      {
+        showAddLegend && (
+          <div className="bg-black bg-opacity-90 fixed top-0 left-0 h-screen w-full flex items-center justify-center" onClick={() => setShowAddLegend(false)}>
+            <div className="bg-black border text-white p-4 rounded-xl" onClick={(e) => e.stopPropagation()}>
+              <form
+                action={(formData: FormData) => {
+                  const legend = formData.get("legend") as string;
+                  const color = formData.get("color") as string;
+                  setCategories({ ...categories, legend: [...categories.legend, `${color}>>${legend}`] });
+                  setShowAddLegend(false);
+                }}
+                className="flex flex-col gap-4"
+              >
+                <div className="flex flex-col gap-2">
+                  <label className="font-bold cursor-pointer" htmlFor="legend">Legenda</label>
+                  <input type="text" name="legend" id="legend" className="rounded-xl p-2 border bg-black outline-blue-800" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="font-bold cursor-pointer" htmlFor="color">Cor</label>
+                  <input type="color" name="color" id="color" className="w-full bg-black outline-blue-800 h-12 cursor-pointer hover:scale-95" />
+                </div>
+                <button className="bg-blue-900 hover:bg-blue-800 rounded-full px-4 py-2 text-white w-full">Adicionar</button>
+              </form>
+            </div>
+          </div>
+        )
+      }
 
+      {
+        addedClipboard ? (
+          <BsClipboard2Check className="fixed bottom-2 right-2 text-green-50 text-4xl hover:scale-95 cursor-pointer" />
+        ) : (
+          <BsClipboard2 className="fixed bottom-2 right-2 text-white text-4xl hover:scale-95 cursor-pointer" onClick={() => {
+            setAddedClipboard(true);
+            addToClipboard();
+            setTimeout(() => {
+              setAddedClipboard(false)
+            }, 2000);
+          }} />
+        )
+      }
     </main>
   );
 }
 
-function Controls({ name, color, currentCategory, previousCategory, nextCategory, rowIndex, changeCategory, removeElement }: { name: string, color?: string, currentCategory: string, previousCategory?: string, nextCategory?: string, rowIndex: number, changeCategory: Function, removeElement: Function }) {
+function Legend({ name, currentCategory, rowIndex, removeElement }: { name: string, currentCategory: string, rowIndex: number, removeElement: Function }) {
   if (name === "") return <></>;
   return (
-    <span className="text-center flex flex-col items-center justify-center px-16 py-2 rounded-xl text-white relative group border break-words max-w-[300px] mx-auto" style={{ backgroundColor: color ? color : "#000" }}>
-      <span>{name}</span>
+    <span className="text-center flex flex-col items-center justify-center px-16 py-2 rounded-xl text-white relative group border break-words max-w-[300px] mx-auto">
+      <span className="flex gap-2 items-center">
+        <span style={{ backgroundColor: name.split(">>").length === 1 ? "#111" : name.split(">>")[0] }} className="w-4 h-4 block border"></span>
+        <span>{name.split(">>").length === 1 ? name.split(">>")[0] : name.split(">>")[1]}</span>
+      </span>
+      <span className="hover:scale-105 cursor-pointer absolute top-1/2 right-8 -translate-y-1/2 group-hover:opacity-100 opacity-0" onClick={() => removeElement(currentCategory, rowIndex)}><BsTrash /></span>
+    </span>
+  )
+}
+
+function Controls({ name, currentCategory, previousCategory, nextCategory, rowIndex, changeCategory, removeElement }: { name: string, currentCategory: string, previousCategory?: string, nextCategory?: string, rowIndex: number, changeCategory: Function, removeElement: Function }) {
+  if (name === "") return <></>;
+  return (
+    <span className="text-center flex flex-col items-center justify-center px-16 py-2 rounded-xl text-white relative group border break-words max-w-[300px] mx-auto" style={{ backgroundColor: name.split(">>").length === 1 ? "#111" : name.split(">>")[0] }}>
+      <span>{name.split(">>").length === 1 ? name.split(">>")[0] : name.split(">>")[1]}</span>
       {
         previousCategory && (
           <span className="absolute top-1/2 left-2 -translate-y-1/2 hover:scale-105 cursor-pointer group-hover:opacity-100 opacity-0" onClick={() => changeCategory(currentCategory, rowIndex, previousCategory)}><BsArrowLeft /></span>
